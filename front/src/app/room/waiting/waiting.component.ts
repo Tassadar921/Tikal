b6dbe3aa4a2598315f0f0b33b5dac689b33d9bca
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2022-12-28 16:03:49 +0100

    confirmation leave room

--- front/src/app/room/waiting/waiting.component.ts
+++ front/src/app/room/waiting/waiting.component.ts
@@ -3,6 +3,7 @@ import {SocketsService} from '../../shared/services/sockets.service';
 import { Clipboard } from '@capacitor/clipboard';
 import {ToastService} from '../../shared/services/toast.service';
 import {CookiesService} from '../../shared/services/cookies.service';
+import {ActionSheetController} from '@ionic/angular';
 
 @Component({
   selector: 'app-waiting',
@@ -14,7 +15,8 @@ export class WaitingComponent implements OnInit {
   constructor(
     public socketsService: SocketsService,
     private toastService: ToastService,
-    public cookiesService: CookiesService
+    public cookiesService: CookiesService,
+    private actionSheetController: ActionSheetController
   ) { }
 
   ngOnInit() {}
@@ -22,6 +24,27 @@ export class WaitingComponent implements OnInit {
   copyID = async () => {
     await Clipboard.write({string: this.socketsService.roomID});
     await this.toastService.displayToast('Copied to clipboard', 3000, 'bottom');
+  };
+
+  leaveRoom = async () => {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Leave room ?',
+      buttons: [
+        {
+          text: 'Yes',
+          icon: 'exit',
+          handler: () => {
+            this.socketsService.leaveRoom();
+          }
+        },
+        {
+          text: 'No',
+          icon: 'close',
+        },
+      ],
+    });
+
+    await actionSheet.present();
   };
 
 }
