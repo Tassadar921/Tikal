30bef3075384240d310d8a950f29bc17fec505b8
Tassadar921 <80831213+Tassadar921@users.noreply.github.com>
2023-01-10 16:16:09 +0100

    patch hashtag in URL

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -9,30 +9,33 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: 'room',
-    loadChildren: () => import('./room/room.module').then( m => m.RoomPageModule)
+    loadChildren: () => import('./room/room.module').then(m => m.RoomPageModule)
   },
   {
     path: 'conf-account',
-    loadChildren: () => import('./conf-account/conf-account.module').then( m => m.ConfAccountPageModule)
+    loadChildren: () => import('./conf-account/conf-account.module').then(m => m.ConfAccountPageModule)
   },
   {
     path: 'connection',
-    loadChildren: () => import('./connection/connection.module').then( m => m.ConnectionPageModule)
+    loadChildren: () => import('./connection/connection.module').then(m => m.ConnectionPageModule)
   },
   {
     path: 'reset-password',
-    loadChildren: () => import('./reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
+    loadChildren: () => import('./reset-password/reset-password.module').then(m => m.ResetPasswordPageModule)
   },
 
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+      useHash: false,
+    })
   ],
   exports: [RouterModule]
 })
